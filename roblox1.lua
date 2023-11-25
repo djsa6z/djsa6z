4cb83fef303af6c49f6ac4908ef29d489443d1a3
djsa6z <151990140+djsa6z@users.noreply.github.com>
2023-11-26 06:18:56 +0700

    Create roblox1.lua

---
+++ roblox1.lua
@@ -0,0 +1,6 @@
+-- Jujutsu Chronicles Script|Viet Nam Is Number One
+
+-- Jujutsu Chronicles
+if game.PlaceId == 11200494415 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/djsa6z/djsa6z/main/roblox2.lua"))()
+end
